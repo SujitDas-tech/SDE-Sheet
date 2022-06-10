4c6824dbc62d668be80609511f42a0478c9b09cf
Sujit Das <63442342+SujitDas-tech@users.noreply.github.com>
2022-06-11 01:11:32 +0530

    Create Merge Two Sorted Linked Lists.java

---
+++ Merge Two Sorted Linked Lists.java
@@ -0,0 +1,43 @@
+import java.util.* ;
+import java.io.*; 
+/************************************************************
+
+    Following is the linked list node structure:
+    
+    class LinkedListNode<T> {
+        T data;
+        LinkedListNode<T> next;
+
+        public LinkedListNode(T data) {
+            this.data = data;
+        }
+    }
+
+************************************************************/
+
+public class Solution {
+	public static LinkedListNode<Integer> sortTwoLists(LinkedListNode<Integer> first, LinkedListNode<Integer> second) {
+		// Write your code here.
+        LinkedListNode<Integer> ans = new LinkedListNode(-1);
+        LinkedListNode<Integer> temp = ans;
+        while(first!=null && second!=null){
+            if(first.data <= second.data){
+                temp.next = first;
+                first=first.next;
+                temp=temp.next;
+            }
+            else{
+                temp.next = second;
+                second = second.next;
+                temp=temp.next;
+            }
+        }
+        if(first!=null){
+            temp.next=first;
+        }
+        if(second!=null){
+            temp.next = second;
+        }
+        return ans.next;
+	}
+}
